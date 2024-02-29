eddf18502ace8b15c70fd324914a9aec24553a1f
Seth Oketch <159660376+50seth@users.noreply.github.com>
2024-02-29 13:55:06 +0300

    PLP DATA TYPES .PY

---
+++ Data types lesson .py
@@ -0,0 +1,39 @@
+a=100
+b=3.14
+#print(a*b)
+c=5+7j
+#print(a+b-c)
+d=False
+#print (d)
+#print(a/b)
+str='Welcome to PLP Academy'
+#print(str)
+#print(str[0])
+#print(str[-1])
+#print (str[4:10])
+#plp='welcome to plp academy python class'
+#print(str+plp)
+#print(plp*4)
+Shopping_List=['bread' 'milk',20.5,40.5]
+#print(Shopping_List)
+Building_list ="[450,'wheelbarrows',180 'shovels','bread','milk']"
+#print(Building_list)
+#print(Shopping_List[0])
+#print(Shopping_List[4:5])
+#print(Shopping_List+Building_list)
+#print(Shopping_List*2)
+plp_tuple= 'abcd',786 ,2.23,'john',70.2
+print(plp_tuple)
+tiny_tuple =('John',5+7j)
+#print(tiny_tuple)
+#print(tiny_tuple+plp_tuple)
+dict={}
+dict['one']={"this is one"}
+dict[2]={'name': 'john','code':6734, 'dept':1007}
+print(dict)['one']
+print(dict[2])
+tinydict=dict[1]
+print(tinydict)
+print(tinydict.keys)
+print(tinydict.values)
+
